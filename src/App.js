a351cbab9ae807d8d556c51705b336cfe35b9a79
Abolfazl Rezvani Naraqi <rezvani.fontdev@gmail.com>
2022-09-09 05:59:43 +0430

    chore: update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import Canvas from "./treemap";
-// import Backup from "./treemap/backup";
+import Treemap from "./treemap";
+import jsonData from "./hooks/data.json";
+import { useState } from "react";
+
+export const convertDataForTreeMap = (object) => {
+  const array = object.sectors;
+
+  const newArray = array
+    .map((item) => {
+      return {
+        code: item.code,
+        name: item.name,
+        value: item.instruments.reduce((prev, currnet) => {
+          return prev + currnet.tradeSummary.totalValue;
+        }, 0),
+        children: item.instruments
+          .map((elem) => {
+            return {
+              name: elem.name,
+              detailName: elem.detailName,
+              category: item.code,
+              ...elem.tradeSummary,
+            };
+          })
+          .sort((a, b) => b.totalValue - a.totalValue),
+      };
+    })
+    .sort((a, b) => b.value - a.value);
+
+  const outObj = {
+    category: object.title,
+    type: object.type,
+    children: newArray,
+  };
+
+  return outObj;
+};
 
 function App() {
-  return <Canvas />;
+  const [tile, setData] = useState(null);
+
+  const handleHover = (info) => setData(info);
+
+  console.log("tile", tile);
+
+  return (
+    <Treemap data={convertDataForTreeMap(jsonData)} onHover={handleHover} />
+  );
 }
 
 export default App;
